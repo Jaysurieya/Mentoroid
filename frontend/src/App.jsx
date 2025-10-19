bcbe538f8e3ee2eddd8c1ba7cc59b1d85c065dd5
Jaysurieya <jaysurieyayuvaraja@gmail.com>
2025-10-19 11:54:20 +0530

    dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import {Routes,Route} from 'react-router-dom'
 import Landing from "./components/landing/Dotted.jsx";
 import Signup from "./components/authentication/Signup.jsx";
+import Dashboard from "./components/dashboard/dashboard.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
---
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import './Dashboard.css';
+function Dashboard() {
+  return (
+    <div className="container">
+        <h1>Welcome to the Dashboard</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
